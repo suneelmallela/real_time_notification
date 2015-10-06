24211c27b657040acf286a932c3048fa28a0dce0
sunilmallela <sunilm807@gmail.com>
2015-10-06 15:58:45 +0530

    Update urls.py

--- realtimenotif/urls.py
+++ realtimenotif/urls.py
@@ -15,11 +15,14 @@ Including another URLconf
 """
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from sendnotif.views import home, alert #Added by Suneel Mallela
 
 urlpatterns = patterns([ '',
 
-	url(r'^$', 'sendnotif.views.home', name='home'),
-	url(r'^alert$', 'sendnotif.views.alert', name='alert'),
+	#url(r'^$', 'sendnotif.views.home', name='home'),
+	#url(r'^alert$', 'sendnotif.views.alert', name='alert'),
+	url(r'^$', home, name='home'),                     			#Addedd by SuneelMallela
+	url(r'^alert$', alert, name='alert'),		     			#Added by Suneel Mallela
 	url(r'^accounts/login/$','django.contrib.auth.views.login',name='login')
     
 	#uncomment the next line to enable the admin:
